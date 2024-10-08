b5fa10efa604fa614514b81e4c1aac55e019e097
Bianka Vasconcelos <bianka.vm03@gmail.com>
2024-10-08 18:41:33 -0400

    feat: adicionar a funcionalidade de DHT no firmware
    Alterado o smartlamp.ino para incluir o GET_HUM e GET_TEMP.

--- smartlamp/smartlamp.ino
+++ smartlamp/smartlamp.ino
@@ -1,84 +1,86 @@
-// Defina os pinos de LED e LDR
-// Defina uma variável com valor máximo do LDR (4000)
-// Defina uma variável para guardar o valor atual do LED (10)
-int ledPin = 18;
-int ledValue = 10;
+#include "DHT.h" // Inclui a biblioteca DHT para leitura de temperatura e umidade
 
-int ldrPin = 4;
-// Faça testes no sensor ldr para encontrar o valor maximo e atribua a variável ldrMax
-int ldrMax = 4000;
+#define DHT_PIN 0 // Define o pino utilizado para o sensor DHT
+#define DHT_TYPE DHT11 // Define o tipo do sensor DHT utilizado (DHT11)
+
+int ledPin = 18; // Pino conectado ao LED
+int ledValue = 10; // Valor atual do LED (inicialmente 10%)
+int tempValue = -1; // Valor da temperatura (inicialmente -1)
+int humValue = -1; // Valor da umidade (inicialmente -1)
+int ldrPin = 4; // Pino do sensor LDR
+int ldrMax = 4000; // Valor máximo do LDR para normalização
+int dhtMax = 2000;
+DHT dht(DHT_PIN, DHT_TYPE); // Cria uma instância do sensor DHT
 
 void setup() {
     Serial.begin(9600);
     
     pinMode(ledPin, OUTPUT);
-    pinMode(ldrPin, INPUT);
+    pinMode(ldrPin, INPUT); 
     
-    Serial.println("SmartLamp Initialized.");
-    // delay(2000);
-    // processCommand(String("GET_LDR"));
-    // Serial.println(analogRead(ldrPin));
-
-
+    Serial.println("SmartLamp Initialized."); 
+ 
+    dht.begin();
 }
 
-// Função loop será executada infinitamente pelo ESP32
-void loop() {
-    //Obtenha os comandos enviados pela serial 
-    //e processe-os com a função processCommand
-
+void loop() {\
+    // Loop para processar os comandos
     if (Serial.available() > 0) {
-      String str = Serial.readString();
-      str.trim();
-      processCommand(str);
-      // delay(1000);
-      // processCommand(String("GET_LDR"));
+        String str = Serial.readString(); 
+        str.trim(); 
+        processCommand(str); 
     }
-
-
 }
-
 
 void processCommand(String command) {
-    // compare o comando com os comandos possíveis e execute a ação correspondente     
+    // Compara o comando com os comandos possíveis e executa a ação correspondente
     Serial.print("command :");
-    Serial.println(command);
+    Serial.println(command); 
 
+    // Responde com o valor do LDR
     if (command.equals(String("GET_LDR"))){
         Serial.printf("RES GET_LDR %d\n", ldrGetValue());
     }
+    // Responde com o valor atual do LED
     else if (command.equals(String("GET_LED"))){
         Serial.printf("RES GET_LED %d\n", ledValue);
     }
+    // Responde com o valor da temperatura
+    else if (command.equals(String("GET_TEMP"))){
+        Serial.printf("RES GET_TEMP %d\n", dhtGetValue());
+    }
+    // Aqui responde com o valor da umidade
+    else if (command.equals(String("GET_HUM"))){
+        Serial.printf("RES GET_HUM %d\n", dhtGetValue());
+    }
+    // Definir o valor do LED
     else if (command.substring(0, 7).equals(String("SET_LED"))){
-        int valor_led = command.substring(7).toInt();
+        int valor_led = command.substring(7).toInt(); // Obtém o valor após "SET_LED"
+        // Verifica se o valor está dentro do intervalo permitido
         if (valor_led >= 0 && valor_led <= 100){
-            ledUpdate(valor_led); // atualizar led 
-            Serial.printf("RES SET_LED 1\n");
+            ledUpdate(valor_led); // Atualiza o LED com o novo valor
+            Serial.printf("RES SET_LED 1\n"); // Confirma a atualização
         }
         else{
-            Serial.printf("RES SET_LED -1\n");
+            Serial.printf("RES SET_LED -1\n"); // Responde com erro
         }
     }
 }
 
 // Função para atualizar o valor do LED
 void ledUpdate(int valor) {
-    // Valor deve convertar o valor recebido pelo comando SET_LED para 0 e 255
-    // Normalize o valor do LED antes de enviar para a porta correspondente
-    
-    int valor_led_norm = map(valor, 0, 100, 0, 255);
-    ledValue = valor;
-    analogWrite(ledPin, valor_led_norm);
-
+    // Converte o valor recebido pelo comando SET_LED de 0-100 para 0-255
+    int valor_led_norm = map(valor, 0, 100, 0, 255); // normaliza o valor
+    ledValue = valor; // att o valor do LED
+    analogWrite(ledPin, valor_led_norm); // escreve o valor no pino do LED
 }   
 
 // Função para ler o valor do LDR
 int ldrGetValue() {
-    // Leia o sensor LDR e retorne o valor normalizado entre 0 e 100
-    // faça testes para encontrar o valor maximo do ldr (exemplo: aponte a lanterna do celular para o sensor)       
-    // Atribua o valor para a variável ldrMax e utilize esse valor para a normalização
-    
-    return map(analogRead(ldrPin), 0, ldrMax, 0, 100);
+    return map(analogRead(ldrPin), 0, ldrMax, 0, 100); // normaliza o valor lido
+}
 
+
+int dhtGetValue(){
+  return map(analogRead(DHT_PIN), 0, dhtMax, 0, 100);
 }
